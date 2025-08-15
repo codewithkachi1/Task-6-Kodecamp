963a7ca0d212daaa4e05ab19e376b0d7d2aa26e0
Quintessa <179593529+QuintessaCodes@users.noreply.github.com>
2025-08-15 07:55:41 +0100

    created functions for authentication and authorization

---
+++ task2/auth.py
@@ -0,0 +1,15 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from utils import hash_password, load_data
+
+security = HTTPBasic()
+
+def authenticate_user(credentials: HTTPBasicCredentials = Depends(security), filename="users.json"):
+    users = load_data(filename)
+    user = users.get(credentials.username)
+    if not user or user["password"] != hash_password(credentials.password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    return user
+
+def is_admin(user):
+    return user["role"] == "admin"
